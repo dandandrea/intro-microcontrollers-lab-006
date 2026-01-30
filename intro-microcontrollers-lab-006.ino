352f704fcdf92868c0181cfcec09eb9f2f4d43ec
Dan D'Andrea <dan.dandrea@gmail.com>
2026-01-29 22:39:10 -0500

    Remove game aspects

--- intro-microcontrollers-lab-006.ino
+++ intro-microcontrollers-lab-006.ino
@@ -10,24 +10,10 @@
 
 #define OLED_I2C_ADDRESS 0x3C
 
+#define SCREEN_AREA_OFFSET_HEIGHT_PIXELS 18
+
 Adafruit_SSD1306 oled(OLED_WIDTH_PIXELS, OLED_HEIGHT_PIXELS, &Wire, -1);
 Adafruit_MPU6050 mpu;
-
-#define GAME_MAX_SECONDS 60
-#define TARGET_MATCH_THRESHOLD_PIXELS 5
-
-uint32_t score = 0;
-uint32_t gameStartMilliseconds = 0;
-uint8_t targetX = 0;
-uint8_t targetY = 0;
-
-void flashScreenNTimes(uint16_t numFlashes);
-void gameReset(bool flashScreen);
-void nextDotLocation();
-int32_t getSecondsRemaining();
-void redrawMainScreenArea();
-
-#define PLAY_AREA_OFFSET_HEIGHT_PIXELS 18
 
 void setup() {
   // put your setup code here, to run once:
@@ -84,11 +70,6 @@ void setup() {
   oled.clearDisplay();
   oled.display();
 
-  randomSeed(analogRead(0));
-
-  nextDotLocation();
-  gameReset(false);
-
   Serial.println(F("[setup()] Done"));
 }
 
@@ -121,110 +102,8 @@ void loop() {
   int16_t xInt = x;
   int16_t yInt = y;
 
-  redrawMainScreenArea();
-  oled.drawCircle(xInt, yInt + PLAY_AREA_OFFSET_HEIGHT_PIXELS, 2, WHITE);
-  oled.drawCircle(targetX, targetY, 1, WHITE);
-  oled.display();
-
-  bool match = false;
-  if (abs(xInt - targetX) <= TARGET_MATCH_THRESHOLD_PIXELS && abs((yInt + PLAY_AREA_OFFSET_HEIGHT_PIXELS) - targetY) <= TARGET_MATCH_THRESHOLD_PIXELS) match = true;
-
-  if (match)
-  {
-    flashScreenNTimes(3);
-    score++;
-    nextDotLocation();
-
-    redrawMainScreenArea();
-    oled.drawCircle(xInt, yInt + PLAY_AREA_OFFSET_HEIGHT_PIXELS, 2, WHITE);
-    oled.drawCircle(targetX, targetY, 1, WHITE);
-    oled.display();
-  }
-
-  if (getSecondsRemaining() <= 0)
-  {
-    gameReset(true);
-    nextDotLocation();
-
-    redrawMainScreenArea();
-    oled.drawCircle(xInt, yInt + PLAY_AREA_OFFSET_HEIGHT_PIXELS, 2, WHITE);
-    oled.drawCircle(targetX, targetY, 1, WHITE);
-    oled.display();
-  }
-}
-
-void flashScreenNTimes(uint16_t numFlashes)
-{
-  for (uint16_t i = 0; i < numFlashes; i++)
-  {
-    oled.invertDisplay(true);
-    oled.display();
-    delay(50);
-    oled.invertDisplay(false);
-    oled.display();
-    delay(50);
-  }
-}
-
-void gameReset(bool flashScreen)
-{
-  if (flashScreen)
-  {
-    flashScreenNTimes(10);
-
-    oled.clearDisplay();
-    oled.setTextColor(WHITE);
-    oled.setCursor(0, 16);
-    oled.println(F("Game Over"));
-    oled.println(F(""));
-    oled.print(F("Score: "));
-    oled.print(score);
-    oled.display();
-
-    delay(2500);
-  }
-
-  gameStartMilliseconds = millis();
-  score = 0;
-}
-
-void nextDotLocation()
-{
-  // +/- 2 so that we don't draw directly on the border
-  targetX = (uint8_t)random(2, OLED_WIDTH_PIXELS - 2);
-  targetY = (uint8_t)random(PLAY_AREA_OFFSET_HEIGHT_PIXELS + 2, OLED_HEIGHT_PIXELS - 2);
-
-  uint8_t verticalCrosshairX0 = targetX;
-  uint8_t verticalCrosshairY0 = PLAY_AREA_OFFSET_HEIGHT_PIXELS;
-
-  uint8_t verticalCrosshairX1 = targetX;
-  uint8_t verticalCrosshairY1 = OLED_HEIGHT_PIXELS;
-
-  uint8_t horizontalCrosshairX0 = 0;
-  uint8_t horizontalCrosshairY0 = targetY;
-
-  uint8_t horizontalCrosshairX1 = OLED_WIDTH_PIXELS;
-  uint8_t horizontalCrosshairY1 = targetY;
-
-  redrawMainScreenArea();
-  oled.drawLine(verticalCrosshairX0, verticalCrosshairY0, verticalCrosshairX1, verticalCrosshairY1, WHITE);
-  oled.drawLine(horizontalCrosshairX0, horizontalCrosshairY0, horizontalCrosshairX1, horizontalCrosshairY1, WHITE);
-  oled.display();
-
-  delay(500);
-}
-
-void redrawMainScreenArea()
-{
   oled.clearDisplay();
-  oled.drawRect(0, PLAY_AREA_OFFSET_HEIGHT_PIXELS, OLED_WIDTH_PIXELS, OLED_HEIGHT_PIXELS - PLAY_AREA_OFFSET_HEIGHT_PIXELS, WHITE);
-  oled.setCursor(0, 0);
-  oled.print(score);
-  oled.setCursor(OLED_WIDTH_PIXELS / 2, 0);
-  oled.print(getSecondsRemaining());
-}
-
-int32_t getSecondsRemaining()
-{
-  return GAME_MAX_SECONDS - ((millis() - gameStartMilliseconds) / 1000);
+  oled.drawRect(0, SCREEN_AREA_OFFSET_HEIGHT_PIXELS, OLED_WIDTH_PIXELS, OLED_HEIGHT_PIXELS - SCREEN_AREA_OFFSET_HEIGHT_PIXELS, WHITE);
+  oled.drawCircle(xInt, yInt + SCREEN_AREA_OFFSET_HEIGHT_PIXELS, 2, WHITE);
+  oled.display();
 }
